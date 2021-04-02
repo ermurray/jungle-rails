49b8313b5a60196bd7bfd827312f148ab7e11bee
ermurray <ermurray31@gmail.com>
2021-04-02 02:38:41 +0000

    add user login and auth

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class Admin::DashboardController < ApplicationController
+  before_filter :authorize
   http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASSWORD']
   def show
     @product_count = Product.count
