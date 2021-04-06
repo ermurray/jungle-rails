1629531fe78055b9d34a96afbc31b82ae6ea4ed4
ermurray <ermurray31@gmail.com>
2021-04-06 23:57:28 +0000

    change users table name column to first_name, adjust views model and controllers accordingly

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 
   has_secure_password
   
-  validates :name, presence: true
+  validates :firts_name, presence: true
   validates :email, presence: true
   validates :password, presence: true
   validates :password, length: {minimum: 5}
