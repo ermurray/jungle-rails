7398a139191e02baf9994f530228ac7c5dc0b7e5
ermurray <ermurray31@gmail.com>
2021-04-07 00:04:16 +0000

    add migration to add user last_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
 
   has_secure_password
   
-  validates :firts_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :email, presence: true
   validates :password, presence: true
   validates :password, length: {minimum: 5}
